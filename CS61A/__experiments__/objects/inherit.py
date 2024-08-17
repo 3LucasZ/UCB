b7758c1fd65f3c33afcfb9b798533d5ecf89ee02
3LucasZ <lucas.zheng@warriorlife.net>
2024-08-17 16:48:02 -0700

    Update inherit.py

--- CS61A/__experiments__/objects/inherit.py
+++ CS61A/__experiments__/objects/inherit.py
@@ -3,15 +3,22 @@ class Object:
     def __repr__(self):
         return f'height:{self.height}'
 class Human(Object):
-    height = 2
+    def __init__(self):
+        self.height += 1
     def __repr__(self):
-        return super().__repr__
+        return super().__repr__()
 class Soldier(Human):
     height = 3
+    def __init__(self):
+        Object.height = 4
 
+
+
+print(Object.height, Human.height, Soldier.height)
 object = Object()
+object.height = 100
 human = Human()
 soldier = Soldier()
-
+print(Object.height, Human.height, Soldier.height)
 print(object, human, soldier)
 
